85508ad415ece94d60afc8dfe75029982cd24041
srdzanK <srdzan.kostenarov@gmail.com>
2026-08-03 15:12:49 +0200

    Clean some files

--- build/Desktop_Qt_6_10_0_MSVC2022_64bit_Release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_sqlite_visual_editor_FILE C:/Users/PC/Documents/GitHub/SQLite-Editor/build/Desktop_Qt_6_10_0_MSVC2022_64bit_Release/sqlite_visual_editor.exe)
-set(__QT_DEPLOY_TARGET_sqlite_visual_editor_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_sqlite_visual_editor_RUNTIME_DLLS C:/Qt/6.10.0/msvc2022_64/bin/Qt6Sql.dll;C:/Qt/6.10.0/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.10.0/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.10.0/msvc2022_64/bin/Qt6Core.dll)
--- build/Desktop_Qt_6_10_0_MSVC2022_64bit_Release/CMakeFiles/sqlite_visual_editor.dir/resources/winsqlite.i
+++
@@ -1,2 +0,0 @@
-#line 1 "C:\\Users\\PC\\Documents\\GitHub\\SQLite-Editor\\resources\\winsqlite.rc"
-IDI_ICON1 ICON "app_icon.ico"
